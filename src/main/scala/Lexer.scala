66130fb4d33cc7475a7abad5715f88e3fc027622
Tim Stewart <tim.j.stewart@gmail.com>
2022-08-01 20:25:32 -0600

    can parse bools.  Needs refactoring..

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -18,51 +18,125 @@ case class COLON(pos: Position) extends Token(":", pos)
 case class CHARACTER(value: Character, pos: Position)
     extends Token(value.toString, pos)
 case class STRING(value: String, pos: Position) extends Token(value, pos)
+case class BOOL_TRUE(pos: Position) extends Token("true", pos)
+case class BOOL_FALSE(pos: Position) extends Token("false", pos)
+case class NUMBER(value: Double, pos: Position) extends Token(value.toString, pos)
 
 case class EOF(pos: Position) extends Token("EOF", pos)
+
+class LexerError(message: String) extends Exception(message)
 
 object Lexer {
 
   import scala.io.Source
 
-  def tokenize(input: Source): Either[Exception, List[Token]] = {
+  def tokenize(input: Source): Either[String, List[Token]] = {
     var curPos = Position(1, 0)
-    Right(input.map { c =>
-      c match {
-        case '\n' =>
-          val newLinePos = curPos.copy(column = curPos.column + 1)
-          curPos = curPos.moveByChar(c)
-          NEWLINE(newLinePos)
-        case _ =>
-          curPos = curPos.moveByChar(c)
-          c match {
-            // case '"' => {
-            //   val origPos = curPos.copy()
-            //   val (string,newPos) = tokenizeString(input, origPos)
-            //   curPos = newPos
-            //   STRING(string, origPos)
-            // }
-            case ',' => COMMA(curPos)
-            case '[' => OPEN_SQUARE_BRACKET(curPos)
-            case ']' => CLOSE_SQUARE_BRACKET(curPos)
-            case '{' => OPEN_CURLY_BRACE(curPos)
-            case '}' => CLOSE_CURLY_BRACE(curPos)
-            case ':' => COLON(curPos)
-            case _   => CHARACTER(c, curPos)
-          }
-      }
-    }.toList :+ EOF(curPos.copy(column = curPos.column + 1)))
+    try {
+      Right(input.map { c =>
+        c match {
+          case '\n' =>
+            val newLinePos = curPos.copy(column = curPos.column + 1)
+            curPos = curPos.moveByChar(c)
+            NEWLINE(newLinePos)
+          case _ =>
+            curPos = curPos.moveByChar(c)
+            c match {
+              case '"' =>
+                val origPos = curPos.copy()
+                val (string, newPos) = tokenizeString(input, curPos)
+                curPos = newPos
+                STRING(string, origPos)
+              case ',' => COMMA(curPos)
+              case '[' => OPEN_SQUARE_BRACKET(curPos)
+              case ']' => CLOSE_SQUARE_BRACKET(curPos)
+              case '{' => OPEN_CURLY_BRACE(curPos)
+              case '}' => CLOSE_CURLY_BRACE(curPos)
+              case ':' => COLON(curPos)
+              case 't' =>
+                val origPos = curPos.copy()
+                tokenizeBool(input, c, curPos, "true") match {
+                  case Right(Tuple2(token, newPos)) =>
+                    curPos = newPos
+                    token
+                  case Left(error) => throw new LexerError(error)
+                }
+              case 'f' =>
+                val origPos = curPos.copy()
+                tokenizeBool(input, c, curPos, "false") match {
+                  case Right(Tuple2(token, newPos)) =>
+                    curPos = newPos
+                    token
+                  case Left(error) => throw new LexerError(error)
+                }
+              case _ => CHARACTER(c, curPos)
+            }
+        }
+      }.toList :+ EOF(curPos.copy(column = curPos.column + 1)))
+    } catch {
+      case ex: LexerError => Left(ex.getMessage)
+    }
   }
 
-  // private def tokenizeString(input: Iterator[Char], startPos: Position): Tuple2[String, Position] = {
-  //   val result = new StringBuffer()
-  //   var escaped = false
+  private def tokenizeString(
+      input: Iterator[Char],
+      startPos: Position
+  ): Tuple2[String, Position] = {
+    val result = new StringBuffer()
+    var curPos = startPos.copy()
+    var done = false
+    var escaped = false
+    do {
+      if (input.hasNext) {
+        val c = input.next()
+        curPos = curPos.moveByChar(c)
 
-  //   do {
-  //     val c = input.next()
+        if (c == '"' && !escaped) {
+          done = true
+        } else {
+          result.append(c)
+        }
 
-  //   } while(false)
+        escaped = false
 
-  //   ("", startPos.copy(column = startPos.column + 1))
-  // }
+        if (c == '\\') {
+          escaped = true
+        }
+
+      } else {
+        done = true
+      }
+    } while (input.hasNext && !done)
+    (result.toString, curPos)
+  }
+
+  private def tokenizeBool(
+      input: Iterator[Char],
+      firstChar: Char,
+      startPos: Position,
+      literal: String
+  ): Either[String, Tuple2[Token, Position]] = {
+    require(firstChar == literal(0))
+    var pos = startPos.copy()
+    try {
+      for (char <- literal.drop(1)) {
+        val readChar = input.next()
+        pos = pos.moveByChar(readChar)
+        if (readChar != char) {
+          throw new Exception(
+            "mismatch: expected: %s, read: %s".format(char, readChar)
+          )
+        }
+      }
+      literal match {
+        case "true"  => Right(BOOL_TRUE(startPos), pos)
+        case "false" => Right(BOOL_FALSE(startPos), pos)
+        case _       => Left("failed to tokenize boolean")
+      }
+    } catch {
+      case ex: Exception =>
+        println("EX: %s".format(ex.getMessage()))
+        Left(ex.getMessage())
+    }
+  }
 }
--- src/test/scala/LexerSpec.scala
+++ src/test/scala/LexerSpec.scala
@@ -6,37 +6,93 @@ import org.scalatest.matchers.should._
 import scala.io.Source
 
 class LexerSpec extends AnyFlatSpec with should.Matchers {
- "A lexer" should "return EOF for an empty stream" in {
-   Lexer.tokenize(Source.fromString("")).right.get shouldBe List(EOF(Position(1,1)))
- }
 
- it should "return COMMA, EOF" in {
-    Lexer.tokenize(Source.fromString(",")).right.get shouldBe List(
-      COMMA(Position(1,1)),
-      EOF(Position(1,2))
+  "A lexer" should "return EOF for an empty stream" in {
+    Lexer.tokenize(Source.fromString("")).right.get shouldBe List(
+      EOF(Position(1, 1))
     )
   }
 
- it should "return open and close square brackets, EOF" in {
-     Lexer.tokenize(Source.fromString("[]")).right.get shouldBe List(
-       OPEN_SQUARE_BRACKET(Position(1,1)),
-       CLOSE_SQUARE_BRACKET(Position(1,2)),
-       EOF(Position(1,3))
-     )
-   }
- it should "return open close square brackets on different lines, EOF" in {
-      Lexer.tokenize(Source.fromString("[\n]")).right.get shouldBe List(
-        OPEN_SQUARE_BRACKET(Position(1,1)),
-        NEWLINE(Position(1,2)),
-        CLOSE_SQUARE_BRACKET(Position(2,1)),
-        EOF(Position(2,2))
+  it should "return COMMA, EOF" in {
+    Lexer.tokenize(Source.fromString(",")).right.get shouldBe List(
+      COMMA(Position(1, 1)),
+      EOF(Position(1, 2))
+    )
+  }
+
+  it should "return open and close square brackets, EOF" in {
+    Lexer.tokenize(Source.fromString("[]")).right.get shouldBe List(
+      OPEN_SQUARE_BRACKET(Position(1, 1)),
+      CLOSE_SQUARE_BRACKET(Position(1, 2)),
+      EOF(Position(1, 3))
+    )
+  }
+
+  it should "return open close square brackets on different lines, EOF" in {
+    Lexer.tokenize(Source.fromString("[\n]")).right.get shouldBe List(
+      OPEN_SQUARE_BRACKET(Position(1, 1)),
+      NEWLINE(Position(1, 2)),
+      CLOSE_SQUARE_BRACKET(Position(2, 1)),
+      EOF(Position(2, 2))
+    )
+  }
+
+  it should "return an empty string" in {
+    Lexer.tokenize(Source.fromString("\"\"")).right.get shouldBe List(
+      STRING("", Position(1, 1)),
+      EOF(Position(1, 3))
+    )
+  }
+
+  it should "return a non-empty string" in {
+    Lexer.tokenize(Source.fromString("\"test\"")).right.get shouldBe List(
+      STRING("test", Position(1, 1)),
+      EOF(Position(1, 7))
+    )
+  }
+
+  it should "return a non-empty string containing a space" in {
+    Lexer.tokenize(Source.fromString("\"test one\"")).right.get shouldBe List(
+      STRING("test one", Position(1, 1)),
+      EOF(Position(1, 11))
+    )
+  }
+
+  it should "return a string with a escaped double quote" in {
+    Lexer.tokenize(Source.fromString(""""foo\"bar"""")).right.get shouldBe List(
+      STRING("""foo\"bar""", Position(1, 1)),
+      EOF(Position(1, 11))
+    )
+  }
+
+  it should "return a string containing a quoted string" in {
+    Lexer
+      .tokenize(Source.fromString(""""foo\"bar\"baz""""))
+      .right
+      .get shouldBe List(
+      STRING("""foo\"bar\"baz""", Position(1, 1)),
+      EOF(Position(1, 16))
+    )
+  }
+
+  it should "return a string even if the quote is never closed" in {
+    Lexer.tokenize(Source.fromString(""""foo""")).right.get shouldBe List(
+      STRING("foo", Position(1, 1)),
+      EOF(Position(1, 5))
+    )
+  }
+
+  it should "return a true value" in {
+      Lexer.tokenize(Source.fromString("true")).right.get shouldBe List(
+        BOOL_TRUE(Position(1, 1)),
+        EOF(Position(1, 5))
       )
     }
 
- // it should "return open any empty string" in {
- //       Lexer.tokenize(Source.fromString("\"\"")).right.get shouldBe List(
- //         STRING("", Position(1, 1)),
- //         EOF(Position(1,3))
- //       )
- //     }
+  it should "return a false value" in {
+        Lexer.tokenize(Source.fromString("false")).right.get shouldBe List(
+          BOOL_FALSE(Position(1, 1)),
+          EOF(Position(1, 6))
+        )
+      }
 }
