d220ea455133ed9cc08b108ca8ee35c6af8bdbb1
Tim Stewart <tim.j.stewart@gmail.com>
2022-08-25 20:50:29 -0600

    First attempt at converting js to json

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,22 +5,37 @@ import json.path.pattern.Property
 object Main {
   def main(args: Array[String]): Unit =
     args.foreach { arg =>
-      extractJavaScript(arg)
+      if (arg.endsWith(".json"))
+        extractJavaScript(arg)
+      else if (arg.endsWith(".js"))
+        mergeJavaScriptIntoJson(arg)
+      else
+        println("huh?")
     }
 
-   private def extractJavaScript(file: String): Unit = {
-      val text = Source.fromFile(file).getLines().mkString("\n")
+  private def extractJavaScript(file: String): Unit = {
+    val text = Source.fromFile(file).getLines().mkString("\n")
 
-      var patterns = Map.empty[Pattern, String]
-      Pattern.parse("**.name").foreach {
-        pattern => patterns = patterns + (pattern -> "name")
-      }
-
-      JsonToJavascriptExtractor.extract(text, patterns) match {
-        case Right(javaScript) => writeToFile(javaScript, file + ".js")
-        case Left(error)       => Left(error)
-      }
+    var patterns = Map.empty[Pattern, String]
+    Pattern.parse("**.name").foreach { pattern =>
+      patterns = patterns + (pattern -> "name")
     }
+
+    JsonToJavascriptExtractor.extract(text, patterns) match {
+      case Right(javaScript) => writeToFile(javaScript, file + ".js")
+      case Left(error)       => println(error)
+    }
+  }
+
+  private def mergeJavaScriptIntoJson(file: String): Unit = {
+    val javaScript = Source.fromFile(file).getLines().mkString("\n")
+    val json = Source.fromFile(file.replace(".json.js", ".json")).getLines().mkString("\n")
+
+    JavaScriptToJsonConverter.merge(javaScript, json) match {
+      case Right(jsonText) => writeToFile(jsonText, file + ".json.out")
+      case Left(error)     => println(error)
+    }
+  }
 
   private def writeToFile(text: String, fileName: String): Unit = {
     import java.io.File
@@ -30,4 +45,3 @@ object Main {
     finally writer.close()
   }
 }
-
